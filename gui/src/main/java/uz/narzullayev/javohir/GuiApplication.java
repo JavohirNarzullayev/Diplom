86eaee02a67b06da43084bb5bbf7e93a5795a79c
Javohir <narzullayevj999@gmail.com>
2022-01-06 23:15:12 +0500

    Remove Datatable

--- gui/src/main/java/uz/narzullayev/javohir/GuiApplication.java
+++ gui/src/main/java/uz/narzullayev/javohir/GuiApplication.java
@@ -2,15 +2,12 @@ package uz.narzullayev.javohir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(
-        basePackages = {"uz.narzullayev.javohir.repository"},
-        repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
+        basePackages = {"uz.narzullayev.javohir.repository"})
 @EnableCaching(proxyTargetClass = true)
 public class GuiApplication {
 
