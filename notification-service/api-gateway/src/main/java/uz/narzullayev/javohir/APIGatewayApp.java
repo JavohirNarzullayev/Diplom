9a2eab4a73faefb788ff0cd5b159159a6aa940d8
Javohir <narzullayevj999@gmail.com>
2022-05-08 22:21:49 +0500

    Zipkin enable

---
+++ notification-service/api-gateway/src/main/java/uz/narzullayev/javohir/APIGatewayApp.java
@@ -0,0 +1,17 @@
+package uz.narzullayev.javohir;/* 
+ @author: Javohir
+  Date: 5/8/2022
+  Time: 7:51 PM*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class APIGatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APIGatewayApp.class, args);
+    }
+}
