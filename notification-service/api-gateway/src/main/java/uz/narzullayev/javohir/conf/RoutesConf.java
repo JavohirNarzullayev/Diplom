c88d29ff5904ded028a5561c76143aaf83eba462
Javohir <narzullayevj999@gmail.com>
2022-05-09 10:32:48 +0500

    Api gateway router configuration completed

---
+++ notification-service/api-gateway/src/main/java/uz/narzullayev/javohir/conf/RoutesConf.java
@@ -0,0 +1,27 @@
+package uz.narzullayev.javohir.conf;/* 
+ @author: Javohir
+  Date: 5/9/2022
+  Time: 10:23 AM*/
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConf {
+
+    //Cloud Gateway Configuration for routes
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("TELEGRAM-SERVICE",
+                        r -> r.path("/api/v1/telegram/**")
+                                .uri("lb://TELEGRAM-SERVICE"))
+                .route("EMAIL-SERVICE",
+                        r -> r.path("/api/v1/email/**")
+                                .uri("lb://EMAIL-SERVICE"))
+                .build();
+    }
+}
