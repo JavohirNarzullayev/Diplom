49e1f14fd6bc4b12d755a13f968889337d470c43
Javohir <narzullayevj999@gmail.com>
2022-06-24 21:07:49 +0500

    flyway configured

--- main-app/main-web/src/main/java/uz/narzullayev/javohir/config/DatabaseConfig.java
+++ main-app/main-web/src/main/java/uz/narzullayev/javohir/config/DatabaseConfig.java
@@ -1,10 +1,16 @@
 package uz.narzullayev.javohir.config;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import uz.narzullayev.javohir.repository.softdeletes.CustomJpaRepositoryFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(
@@ -13,5 +19,20 @@ import uz.narzullayev.javohir.repository.softdeletes.CustomJpaRepositoryFactoryB
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableTransactionManagement
 public class DatabaseConfig {
+    @Bean
+    public Hibernate5Module hibernate5Module() {
+        return new Hibernate5Module();
+    }
+
+    @Bean(name = "flyway", initMethod = "migrate")
+    @Profile({"!test"})
+    public Flyway flywayNotADestroyer(DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .baselineOnMigrate(true)
+                .baselineVersion("10000")
+                .outOfOrder(true)
+                .load();
+    }
 
 }
---
+++ main-app/main-web/src/main/resources/db/migration/V1000__testing.sql
@@ -0,0 +1,8 @@
+DELETE
+FROM plan_teacher;
+DELETE
+FROM quiz;
+DELETE
+FROM file_entity;
+DELETE
+FROM users;
